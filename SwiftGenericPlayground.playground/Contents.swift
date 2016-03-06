e451538a0ce7e71e2ee60772dc0956c92f66cc17
Kan Chen <kan@drchrono.com>
2016-03-05 16:19:02 -0800

    add UnitTest project

--- SwiftGenericPlayground.playground/Contents.swift
+++ SwiftGenericPlayground.playground/Contents.swift
@@ -26,21 +26,6 @@ items.removeLast()
 items.removeLast()
 //items.removeLast()
 
-struct Stack<Item> {
-    var items: [Item] = []
-    mutating func push(x: Item) {
-        items.append(x)
-    }
-    mutating func pop(x: Item) {
-        items.removeLast()
-    }
-}
-
-extension Stack {
-    var topItem: Item? {
-        return items.last
-    }
-}
 
 func findStringIndex(string:String, strings: [String]) -> Int? {
     return strings.indexOf(string)
@@ -60,4 +45,72 @@ protocol Container {
     mutating func append(item: ItemType)
     var count: Int { get }
     subscript(i: Int) -> ItemType{ get }
+}
+
+struct Stack<Item>: Container {
+    var items: [Item] = []
+    mutating func push(x: Item) {
+        items.append(x)
+    }
+    mutating func pop(x: Item) {
+        items.removeLast()
+    }
+    typealias ItemType = Item
+    mutating func append(item: ItemType) {
+        push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i:Int) -> ItemType {
+        return items[i]
+    }
+
+}
+
+extension Stack {
+    var topItem: Item? {
+        return items.last
+    }
+}
+struct IntStack: Container {
+    var items = [Int]()
+    mutating func push(item: Int) {
+        items.append(item)
+    }
+    mutating func pop() -> Int {
+        return items.removeLast()
+    }
+
+//    typealias ItemType = Int
+    mutating func append(item: Int) {
+        self.push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i: Int) -> Int {
+        return items[i]
+    }
+}
+
+func allItemMatch<C1: Container, C2: Container where C1.ItemType == C2.ItemType, C1.ItemType: Equatable>(someContainer:C1, _ anotherContainer: C2) -> Bool {
+    if someContainer.count != anotherContainer.count {
+        return false
+    }
+    for i in 0..<someContainer.count {
+        if someContainer[i] != anotherContainer[i] {
+            return false
+        }
+    }
+    return true
+}
+extension Array: Container {}
+var stackOfString = Stack<String>()
+stackOfString.push("uno")
+stackOfString.push("dos")
+stackOfString.push("tres")
+var arrayOfString = ["uno", "dos", "tres"]
+if allItemMatch(arrayOfString, stackOfString) {
+    print("good")
 }
